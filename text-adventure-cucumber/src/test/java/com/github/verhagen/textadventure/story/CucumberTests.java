8edd9008c507326027ae1703ff7c8e75d428eda8
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2014-12-06 10:03:50 +0100

    Removes tag

--- text-adventure-cucumber/src/test/java/com/github/verhagen/textadventure/story/CucumberTests.java
+++ text-adventure-cucumber/src/test/java/com/github/verhagen/textadventure/story/CucumberTests.java
@@ -4,8 +4,9 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+//, tags = { "@failing" }
 //@Cucumber.Options(format = { "pretty", "html:target/cucumber" }, features = { "src/test/cucumber/" }, tags = { "@smoke" })
-@cucumber.api.CucumberOptions(plugin = { "pretty", "html:target/cucumber" }, features = { "src/cucumber/" }, tags = { "@failing" })
+@cucumber.api.CucumberOptions(plugin = { "pretty", "html:target/cucumber" }, features = { "src/cucumber/" })
 public class CucumberTests {
 
 }
