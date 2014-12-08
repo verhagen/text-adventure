680c2c713e92456e6e6a1d102480e7fc458f3b08
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2014-12-08 10:11:01 +0100

    Renames InsideRoom to ItemInRoom

--- text-adventure-cucumber/src/test/java/com/github/verhagen/textadventure/story/ItemInRoom.java
+++ text-adventure-cucumber/src/test/java/com/github/verhagen/textadventure/story/ItemInRoom.java
@@ -16,7 +16,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class InsideRoom {
+public class ItemInRoom {
 	private IRoom room;
 	private Map<String, IItem> items = new HashMap<>(); 
 
